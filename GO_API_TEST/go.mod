dd7245af22552c178329d1c7b050a85dff12854d
idearslove <idea.bunyalit@gmail.com>
2023-01-25 10:56:50 -0800

    add

---
+++ GO_API_TEST/go.mod
@@ -0,0 +1,15 @@
+module github.com/idearslove/GO_API_TEST
+
+go 1.19
+
+require github.com/onsi/gomega v1.26.0
+
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ GO_API_TEST/user_test.go
@@ -0,0 +1,31 @@
+package goapitest
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+type Video struct {
+	gorm.Model
+	Name string `valid:"required~Name connot be blank"`
+	Url  string `gorm:"uiqueIndex" valid:url"`
+}
+
+func TestVideoVali(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run("Check name not blank", func(t *testing.T) {
+		u := Video{
+			Name: "",
+			Url:  "oo",
+		}
+
+		ok, err := govalidator.ValidateStruct(u)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).ToNot(BeNil())
+		g.Expect(err.Error()).To(Equal("name not blank"))
+	})
+}
